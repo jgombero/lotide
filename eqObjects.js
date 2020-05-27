f895a9b5112ee89d9c98a70fbca9eeccc2210ac4
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-05-27 22:40:18 +0000

    initial commit function compares two objects containing primitive data types and arrays

---
+++ eqObjects.js
@@ -0,0 +1,44 @@
+const eqArrays = (arr1, arr2) => arr1.join() === arr2.join() ? true : false;
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    if ((Array.isArray(object1[key]) && (Array.isArray(object2[key])))) { // if both arrays
+      if (!eqArrays(object1[key], object2[key])) // if eqArrays test results in false
+        return false;
+    } else if (object1[key] !== object2[key]) {
+      console.log('can you see me');
+      return false;
+    }
+  }
+  return true;
+};
+
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// console.log(eqObjects(ab, ba)); // => true
+
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(eqObjects(ab, abc)); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
+
