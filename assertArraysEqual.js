4f556d939dccbd10c2640ad03db91e9acbf5d903
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-05-28 21:17:57 +0000

    got rid of trailing spaces

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,8 @@
 const eqArrays = (arr1, arr2) => arr1.join() === arr2.join() ? true : false;
 
 const assertArraysEqual = (arr1, arr2) => {
-  (eqArrays(arr1, arr2)) 
-    ? console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`) 
+  (eqArrays(arr1, arr2))
+    ? console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`)
     : console.log(`❌❌❌ Assertion Failed: [${arr1}] !== [${arr2}]`);
 };
 
