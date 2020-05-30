ab921c0d32aa5586c508c234ba5bd89240142dd1
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-05-30 00:29:11 +0000

    added compatibility with nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -10,7 +10,22 @@ const assertEqual = function(actual, expected) {
 //  based on a perfect match.
 
 const eqArrays = function(arr1, arr2) {
-  return arr1.join() === arr2.join() ? true : false;
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (const elem in arr1) {
+
+    if (Array.isArray(arr1[elem]) && Array.isArray(arr2[elem])) {
+      return eqArrays(arr1[elem], arr2[elem]);
+
+    } else if (arr1[elem] !== arr2[elem]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, [2, [5, 6]], 3], [1, [2, [5, 6]], 3]), true);
+assertEqual(eqArrays([[1, 2], [5, 6], 3], [1, [2, [5, 6]], 3]), false);
