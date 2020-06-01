cd0c037294166b538f3b5bf4e7dea6befcc15fb6
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-01 20:31:58 +0000

    Dried up code with requires and exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = (arr1, arr2) => arr1.join() === arr2.join() ? true : false;
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   (eqArrays(arr1, arr2))
@@ -6,4 +6,4 @@ const assertArraysEqual = (arr1, arr2) => {
     : console.log(`❌❌❌ Assertion Failed: [${arr1}] !== [${arr2}]`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
