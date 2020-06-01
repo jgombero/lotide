4a00c6d745e0f6f09668ef718bb6ff28f1810d69
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-01 20:53:20 +0000

    Using mocha and chai tests and assertions now

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,17 @@
+const assert = require('chai').assert
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-const example = head([1, 2, 3, 4, 5]);
-assertEqual(example, 1);
+describe('#head', () => {
+
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it('returns "5" for ["5"]', () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it('returns 1 for 1, 2, 3, 4, 5]', () => {
+    assert.strictEqual(head([1, 2, 3, 4, 5]), 1);
+  });
+});
